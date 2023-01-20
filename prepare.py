691899a4543d8a1d42896fc6454bf3518c42a2c7
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2023-01-20 15:01:16 -0600

    modified file

--- prepare.py
+++ prepare.py
@@ -3,14 +3,12 @@ import numpy as np
 import os
 from env import get_connection
 from sklearn.model_selection import train_test_split
-
+import functions as f
+from sklearn.impute import SimpleImputer
 
 def prep_zillow(df):
-    '''
-    This function takes in the zillow df
-    then the data is cleaned and returned
-    '''
-    #change column names to be more readable
+    ''' Prepare zillow data for exploration by taking in a dataframe and returns train, validate, test'''
+     
     df = df.rename(columns={'bedroomcnt':'bedrooms', 
                           'bathroomcnt':'bathrooms', 
                           'calculatedfinishedsquarefeet':'sqr_feet',
@@ -18,15 +16,30 @@ def prep_zillow(df):
                           'taxamount': 'tax_amount',
                           'yearbuilt':'year_built'})
 
-    #drop null values- at most there were 9000 nulls (this is only 0.5% of 2.1M)
-    df = df.dropna()
-
-    #drop duplicates
-    df.drop_duplicates(inplace=True)
+    # removing outliers
+    df = f.remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'sqr_feet', 'property_value',])
     
-    # train/validate/test split
+
+    
+    # drop null values that are left in the lot size data since there are so few
+    df=df.dropna()
+    
+    # converting column datatypes 
+    df.fips = df.fips.astype(object)
+    df.year_built = df.year_built.astype(object)
+    
+    # train/validate/test split and is reproducible due to random_state = 123
     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
-    print(train.shape), print(validate.shape), print(test.shape)
-    return train, validate, test
+    
+    # impute year built using median from train data set and then applied to the validate and test set as well
+    imputer = SimpleImputer(strategy='median')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test    
 
