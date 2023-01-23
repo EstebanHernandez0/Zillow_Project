c051b74dde407ea50415b655fd149bf038f6741a
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2023-01-23 15:35:35 -0600

    Update

--- functions.py
+++ functions.py
@@ -1,58 +1,45 @@
-def get_hist(df):
-    ''' Creates histographs from our continuous variables from the dataset that we acquired'''
+import pandas as pd
+import numpy as np
+import os
+from env import get_connection
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_box(train):
+    '''
+    plots boxplots of continoues variables using the train dataset
+    '''
     
-    # plt.figure(figsize=(16, 3))
+    # List of columns
+    cols= ['bedrooms', 'bathrooms', 'sqr_feet', 'property_value',]
 
-    # here we make our list of columns
-    cols = [col for col in df.columns if col not in ['fips', 'year_built',]]
+    plt.figure(figsize=(16, 3))
 
-    for i, col in enumerate(cols):
+    for x, col in enumerate(cols):
 
-        # i starts at 0, but plot nos should start at 1
-        plot_number = i + 1 
+        plot_number= x + 1 
 
-        # Create subplot.
+        
+        # subplot.
         plt.subplot(1, len(cols), plot_number)
 
-        # Title with column name.
+        
+        # Title name using the column names.
         plt.title(col)
 
-        # Display histogram for column.
-        df[col].hist(bins=5)
+        
+        # boxplot.
+        sns.boxplot(data=train[[col]])
 
-        # Hide gridlines.
+        
+        # turns off the grid lines.
         plt.grid(False)
 
-        # turn off scientific notation
-        plt.ticklabel_format(useOffset=False)
-
+        
+        # proper plot spacing
         plt.tight_layout()
 
     plt.show()
-
-
-
-
-
-def remove_outliers(df, k, col_list):
-    ''' 
-
-    gets rid of outliers from a list of columns in our dataframe
-    then it returns the new dataframe without outliers
-    
-    '''
-    
-    for col in col_list:
-
-        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
         
-        iqr = q3 - q1   # calculate interquartile range
         
-        upper_bound = q3 + k * iqr   # get upper bound
-        lower_bound = q1 - k * iqr   # get lower bound
-
-        # return dataframe without outliers
-        
-        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
-        
-    return df
