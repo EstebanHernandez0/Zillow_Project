34ce5d07b4726dfb57b6001dc5f49495c55db36a
EstebanHernandez0 <EstebanHernandez2024@gmail.com>
2023-01-23 19:40:03 -0600

    Update

--- prepare.py
+++ prepare.py
@@ -10,7 +10,8 @@ def prep_zillow(df):
                           'calculatedfinishedsquarefeet':'sqr_feet',
                           'taxvaluedollarcnt':'property_value', 
                           'taxamount': 'tax_amount',
-                          'yearbuilt':'year_built'})
+                          'yearbuilt':'year_built',
+                          'fips': 'county'})
 
     # removing outliers to make the data easier to use and more trustworthy
     df = drop_outers(df, 1.5, ['bedrooms', 'bathrooms', 'sqr_feet', 'property_value',])
@@ -22,21 +23,24 @@ def prep_zillow(df):
     df.drop(columns= ['Unnamed: 0'], inplace= True)
     
     # converting column datatypes 
-    df.fips = df.fips.astype(object)
-    df.year_built = df.year_built.astype(object)
+    df.county= df.county.astype(object)
+    df.year_built= df.year_built.astype(object)
+    
+    # create column with fips value converted from an integer to the county name string
+    df['county']= df.county.map({6037 : 'los_angeles', 6059 : 'orange', 6111 : 'ventura'})
     
     # train/validate/test split and is reproducible due to random_state = 123
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
-    train, val = train_test_split(train_validate, test_size=.3, random_state=123)
+    train_validate, test= train_test_split(df, test_size= .2, random_state= 123)
+    train, val= train_test_split(train_validate, test_size= .3, random_state =123)
     
     # impute year built using median from train data set and then applied to the validate and test set as well
     imputer = SimpleImputer(strategy='median')
 
     imputer.fit(train[['year_built']])
 
-    train[['year_built']] = imputer.transform(train[['year_built']])
-    val[['year_built']] = imputer.transform(val[['year_built']])
-    test[['year_built']] = imputer.transform(test[['year_built']])  
+    train[['year_built']]= imputer.transform(train[['year_built']])
+    val[['year_built']]= imputer.transform(val[['year_built']])
+    test[['year_built']]= imputer.transform(test[['year_built']])  
     
     
     
@@ -48,7 +52,16 @@ def prep_zillow(df):
     
     return train, val, test    
 
-
+def split_data(df, target=''):
+        train, test = train_test_split(df, 
+                               train_size = 0.8,
+                               random_state=1349,
+                              stratify=df[target])
+        train, val = train_test_split(train,
+                             train_size = 0.7,
+                             random_state=1349,
+                             stratify=train[target])
+        return train, val, test
 
 def drop_outers(df, o, col_list):
     ''' 
